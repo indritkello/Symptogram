e73f51bcd0c4ebd799d008d690d6366a1c687331
= <=>
2020-10-27 21:48:20 +0100

    Inital changes, project structure, technologies, configurations and knowledge

---
+++ README.md
@@ -0,0 +1,17 @@
+## SYMPTOGRAM
+
+This project was made using Expo, React-Native and Firebase. 
+
+## Installation
+
+* **Clone and install packages**
+
+```
+git clone https://proclinicsystem@dev.azure.com/proclinicsystem/Symptogram/_git/Symptogram
+cd Symptogram
+npm install
+expo start
+```
+
+* **Note: In case of missing packages, just run the recommended commands right away.**
+
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD8XlzdW5jjjhL88A9vqYnmcnzA3lb6DEA',
+  authDomain: 'symptogram.firebaseapp.com',
+  databaseURL: 'https://symptogram.firebaseio.com',
+  projectId: 'symptogram',
+  messagingSenderId: '118633347729',
+  appId: '1:118633347729:ios:defd64bfb5fc6ab99d1d30',
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
